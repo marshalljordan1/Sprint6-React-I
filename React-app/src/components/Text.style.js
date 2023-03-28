7358f070777bd37477105555f6baf5739613943e
marshalljordan1 <jordan_marshall@live.co.uk>
2023-03-29 00:12:13 +0200

    complete exercise 3

---
+++ React-app/src/components/Text.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Text = styled.text`
+    display: flex;
+    justify-content: center;
+    width: 97%;
+    padding: 10px;
+    margin: 10px;
+    font-family: 'Times New Roman', Times, serif;
+    border: solid black 2px;
+    border-radius: 50px;
+`
