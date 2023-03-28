17b6e99e918b1c33b55cc316ec5ec5223268d4bb
marshalljordan1 <jordan_marshall@live.co.uk>
2023-03-28 11:38:41 +0200

    final complete ex2

--- React-app/src/sentences.js
+++ React-app/src/sentences.js
@@ -1,19 +1,19 @@
 const sentences = [
     {
         id: 1,
-        line: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+        line: "Our hero was floating through outer space when he spotted a spaceship in the distance"
     }, 
     {
         id: 2, 
-        line: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes."
+        line: "He was curious about the interior of the ship and began to inspect it. He came to a room with two doors."
     }, 
     {
         id: 3, 
-        line: "L'heroi va decidir travessar la porta que el portava a casa"
+        line: "The hero decided to go through the door that led him home"
     }, 
     {
         id: 4, 
-        line: "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ..."
+        line: "Meanwhile, other heroes were not so lucky in their choice ..."
     } 
 ]
 
