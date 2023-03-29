e958cd1d5ede9aef73bed4f6c4e275270b7f0bb4
marshalljordan1 <jordan_marshall@live.co.uk>
2023-03-29 12:30:50 +0200

    complete exercise 4

---
+++ React-app/src/components/Styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`
+
+export const Buttons = styled.button`
+    flex: 1;
+    padding: 10px;
+`
